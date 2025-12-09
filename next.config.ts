f05a3db6af2403ced44fddbd30ea7a3d7fcea36a
Jiaying Lin <jiayinglin646@gmail.com>
2025-12-09 15:21:15 -0500

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 💡 NEW SETTING: Explicitly disable Turbopack for the compiler
-  compiler: {
-    // Setting this to true forces Next.js to use Webpack for the build
-    // where your custom configuration can take effect.
-    // NOTE: This might slightly increase build time compared to Turbopack.
-    reactRemoveProperties: true, // You may already have this or similar settings
-  },
-  
-  // Your existing Next.js configuration settings...
-  reactStrictMode: true,
-
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [
-        ...config.plugins,
-        new PrismaPlugin(),
-      ];
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
