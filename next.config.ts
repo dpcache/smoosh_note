1e13ada47e58bf72c9082773d2aa3ea7a4db928c
Jiaying Lin <jiayinglin646@gmail.com>
2025-12-09 15:07:08 -0500

    zz

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-// next.config.ts
 import type { NextConfig } from "next";
-import { withNextjsMonorepoWorkaround } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // your other config here
+  /* config options here */
 };
 
-export default withNextjsMonorepoWorkaround(nextConfig);
+export default nextConfig;
